c196a2f1b1517a0796bab544d59ef9f63bdb72fa
owen <30269068@bsdc.ac.uk>
2024-11-20 16:18:56 +0000

    added on the fl;y table demo

---
+++ objects/app.js
@@ -0,0 +1,15 @@
+console.log('works')
+const malgames=[
+    {name:'superstar saga',release:'21/11/2003',ign:9},
+    {name:'partners in time',release:'28/11/2005',ign:9},
+    {name:'bowser`s inside story',release:'11/2/2009',ign:9.5},
+    {name:'dream team',release:'30/6/2013',ign:8},
+    {name:'paper jam bros.',release:'3/12/2015',ign:5.9}
+]
+const mal=document.getElementById('marioandluigi')
+for(i=0;i<5;i++){
+    const newgame=document.createElement('tr')
+    newgame.innerHTML=`<td>${malgames[i].name}</td><td>${malgames[i].release}</td><td>${malgames[i].ign}</td>`
+    console.log(newgame)
+    mal.appendChild(newgame)
+}
