d83925d7b74db265a88c0526cd7d5849315276bb
owen <30269068@bsdc.ac.uk>
2024-11-27 16:21:50 +0000

    improved pokemon

--- pokemon/app.js
+++ pokemon/app.js
@@ -1,6 +1,6 @@
 console.log('2025')
 const container=document.querySelector('#container')
-for (i=1;i<=1025;i++){
+for (i=1;i<=11025;i++){
     const newimg=document.createElement('img')
     newimg.src=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`
     container.appendChild(newimg)
